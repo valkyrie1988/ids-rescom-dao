03a49a7003250dc412cc9d865d42e1698ad727b6
Calvin Koh <kohcw1988@gmail.com>
2016-08-25 00:17:23 +0800

    -after merge

--- src/main/java/com/ids/rescom/repositories/AppointmentRepository.java
+++ src/main/java/com/ids/rescom/repositories/AppointmentRepository.java
@@ -1,5 +1,7 @@
 package com.ids.rescom.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.ids.rescom.entities.appointment.Appointment;
 
 @Repository
 public interface AppointmentRepository extends CrudRepository<Appointment, Long>{
-	 
+	public Appointment findById(Long id);
+	public Appointment findByIdAndStatus(Long id, int status);
+	public List<Appointment> findByCreatedByAndStatusOrderByVisitDtDesc(Long id, int status);
 }
